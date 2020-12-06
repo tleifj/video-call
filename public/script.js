95ea85037f0f78effe895cdde70813b3d9078ce9
Travis Johnson <travisjohnson@Traviss-MacBook-Pro-3.local>
2020-12-05 21:16:18 -0800

    Connects to another users video and connects are video to other user

--- public/script.js
+++ public/script.js
@@ -15,7 +15,22 @@ navigator.mediaDevices.getUserMedia({
     // Add our video to screen
     addVideoStream(myVideo, stream);
 
-    
+    // When a call comes in, answer and send stream
+    myPeer.on('call', call => {
+        call.answer(stream);
+
+        // Creates a new video element to add to our page
+        const video = document.createElement('video');
+
+        
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream);
+        })
+    })
+    // When a user is connected, create a call and add video
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream);
+    });
 });
 
 // Connect to Peer server on port 3002
@@ -29,15 +44,32 @@ myPeer.on('open', id => {
 
 })
 
-
-
-
 function addVideoStream(video, stream) {
     // Set video source to the stream
     video.srcObject = stream;
+
     // Once it's loaded, play it
     video.addEventListener('loadedmetadata', () => {
         video.play();
     })
+    // Adds video to grid of videos
     videoGrid.append(video);
+}
+
+function connectToNewUser(userId, stream) {
+    // Creates a PeerJS call
+    const call = myPeer.call(userId, stream);
+
+    // Creates a new video element to add to our page
+    const video = document.createElement('video');
+
+    // On stream, add user's video to our page
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream);
+    })
+
+    // When call ends, remove video element from screen
+    call.on('close', () => {
+        video.remove();
+    })
 }
