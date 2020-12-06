64e7d186874bda8eb6acbf9b58a2988903a10f83
Travis Johnson <travisjohnson@Traviss-MacBook-Pro-3.local>
2020-12-05 20:58:50 -0800

    Adds own video to screen

--- public/script.js
+++ public/script.js
@@ -1,11 +1,43 @@
 const socket = io();
+// Get video grid
+const videoGrid = document.getElementById('video-grid');
+console.log(videoGrid);
+// Create video element
+const myVideo = document.createElement('video');
+// Make created video muted so we don't hear ourselves
+myVideo.muted = true;
+
+// Get the users video and audio
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true,
+}).then(stream => {
+    // Add our video to screen
+    addVideoStream(myVideo, stream);
+
+    
+});
+
+// Connect to Peer server on port 3002
 const myPeer = new Peer(undefined, {
     host: '/',
     port: '3002'
 })
 
-socket.emit('join-room', ROOM_ID, 10);
+myPeer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id);
 
-socket.on('user-connected', userId => {
-    console.log(`User Connected: ${userId}`);
-});
+})
+
+
+
+
+function addVideoStream(video, stream) {
+    // Set video source to the stream
+    video.srcObject = stream;
+    // Once it's loaded, play it
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    })
+    videoGrid.append(video);
+}
